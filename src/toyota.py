a689a3407e136c683df6bd2545171e4ec83d50c8
Robert <rwallhead@gmail.com>
2014-12-28 17:41:28 +1100

    Very basic websockets-supporting flask app

--- src/toyota.py
+++ src/toyota.py
@@ -1,9 +1,21 @@
+# Flask fundamentals
 from flask import Flask
-app = Flask(__name__)
+app = Flask('toyota')
+
+# Some configuration
+app.config['SECRET_KEY'] = 'secret!' # TODO: don't...
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 # 10KB seems fair
+
+# Websockets
+import flask.ext.socketio
+socketio = flask.ext.socketio.SocketIO(app)
 
 @app.route("/")
 def hello():
+    """ The main page...
+    """
     return "Hello World!"
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    socketio.run(app, host='0.0.0.0')
