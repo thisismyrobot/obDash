73f62e5058d6c20ec0399e986856eadf9c774601
Robert <rwallhead@gmail.com>
2014-12-28 17:48:44 +1100

    Rendering the basic template

--- src/toyota.py
+++ src/toyota.py
@@ -1,21 +1,20 @@
-# Flask fundamentals
-from flask import Flask
-app = Flask('toyota')
+import flask.ext.socketio
+import flask
 
-# Some configuration
+
+app = flask.Flask('toyota')
 app.config['SECRET_KEY'] = 'secret!' # TODO: don't...
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 # 10KB seems fair
+socketapp = flask.ext.socketio.SocketIO(app)
 
-# Websockets
-import flask.ext.socketio
-socketio = flask.ext.socketio.SocketIO(app)
 
 @app.route("/")
-def hello():
-    """ The main page...
+def index():
+    """ The main page
     """
-    return "Hello World!"
+    return flask.render_template('index.html')
+    """
 
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0')
+    socketapp.run(app, host='0.0.0.0')
