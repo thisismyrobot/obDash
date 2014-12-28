728c46fde305f93553fc6e285304a57a46b90217
Robert <rwallhead@gmail.com>
2014-12-28 18:34:56 +1100

    Can now set the time via a GET parameter. This will be used by the browsing app device I hope.

--- src/toyota.py
+++ src/toyota.py
@@ -1,8 +1,10 @@
 import flask.ext.socketio
 import flask
+import subprocess
 
 
 app = flask.Flask('toyota')
+app.debug = True
 app.config['SECRET_KEY'] = 'secret!' # TODO: don't...
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 # 10KB seems fair
 socketapp = flask.ext.socketio.SocketIO(app)
@@ -19,6 +21,20 @@ def index():
 def settime():
     """ Set the current time using a GET request.
     """
+    epoc = flask.request.args.get('epoc', None)
+
+    # It has to be a positive integer...
+    try:
+        if int(epoc) < 0:
+            raise Exception('Negative int...')
+    except:
+        return ''
+
+    # Works as 'pi' on an, err, Pi
+    status = subprocess.call('sudo date --set=\'@{}\''.format(epoc),
+                             shell=True)
+
+    return ''
 
 
 if __name__ == "__main__":
