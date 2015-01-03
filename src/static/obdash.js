f00432c89e5e74cf63a057f923a5e7e18ef63844
Robert Wallhead <rwallhead@gmail.com>
2015-01-04 10:50:01 +1100

    Setting offset now refreshes the page

--- src/static/obdash.js
+++ src/static/obdash.js
@@ -4,3 +4,12 @@ function setTime() {
         'epoch': new Date().getTime() / 1000,
     });
 }
+
+function refresh(delay) {
+    if (delay === undefined) {
+        delay = 0;
+    }
+    setTimeout(function() {
+        location.reload(true);
+    }, delay);
+}
