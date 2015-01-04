c7b284d6001c4423ccd4a64e929ebc2d6b87851b
Robert Wallhead <rwallhead@gmail.com>
2015-01-04 11:10:51 +1100

    Module pattern before I leave it too long...

--- src/static/obdash.js
+++ src/static/obdash.js
@@ -1,15 +1,20 @@
-// Sets the server time offset from the client device
-function setTime() {
-    $.post('/time', {
-        'epoch': new Date().getTime() / 1000,
-    });
-}
+var obdash = (function () {
+    return {
 
-function refresh(delay) {
-    if (delay === undefined) {
-        delay = 0;
-    }
-    setTimeout(function() {
-        location.reload(true);
-    }, delay);
-}
+        // Sets the server time offset from the client device
+        setTime: function () {
+            $.post('/time', {
+                'epoch': new Date().getTime() / 1000,
+            });
+        },
+
+        refresh: function(delay) {
+            if (delay === undefined) {
+                delay = 0;
+            }
+            setTimeout(function() {
+                location.reload(true);
+            }, delay);
+        }
+    };
+})();
